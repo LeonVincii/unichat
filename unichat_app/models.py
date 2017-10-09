369bf914aa5e704e0bd794c0cab8315882b20cfd
Leon Vincii <leon.vincii@gmail.com>
2017-10-09 12:46:28 +1100

    Further optimizations on database structure.

--- unichat_app/models.py
+++ unichat_app/models.py
@@ -77,21 +77,14 @@ class Contact(models.Model):
 
 class ChatList(models.Model):
 	user = models.ForeignKey(User, related_name = 'chat_myself')
-	chat_user = models.ForeignKey(User, related_name = 'chat_user')
-	chat_remarkname = models.CharField(max_length = 50, editable = False, null = True, blank = True)
+	chat_user = models.ForeignKey(Contact, related_name = 'chat_user')
 
 	def __str__(self):
-		return self.user.username + ' :-> ' + self.chat_user.username
+		return self.user.username + ' :-> ' + self.chat_user.contact_user.username
 
 	def clean(self):
 		if self.user == self.chat_user:
 			raise ValidationError('Chatting with self is not recommended :D')
-		chat_user_qs = Contact.objects.filter(user = self.user, contact_user = self.chat_user)
-		if not chat_user_qs:
-			raise ValidationError('%(user)s isn\'t your contact', params = {'user': self.chat_user.username})
-		else:
-			chat_user = Contact.objects.get(user = self.user, contact_user = self.chat_user)
-			self.chat_remarkname = chat_user.contact_remarkname
 		if ChatList.objects.filter(user = self.user, chat_user = self.chat_user):
 			raise ValidationError('%(me)s has already had a chat with %(user)s',
-			                      params = {'me': self.user.username, 'user': self.chat_user.username})
+			                      params = {'me': self.user.username, 'user': self.chat_user.contact_user.username})
