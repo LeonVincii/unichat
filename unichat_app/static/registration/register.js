445f56e688d2c249456be9352eb22c507b80b949
Leon Vincii <leon.vincii@gmail.com>
2017-10-13 12:11:45 +1100

    Added validation indicator on signup page; Added reference.html for icons.

--- unichat_app/static/registration/register.js
+++ unichat_app/static/registration/register.js
@@ -1,16 +1,16 @@
-$('#username_input').ready(function() {
+$('#register_form').ready(function() {
+    /* Validates the username. */
     $('#username_input').change(function() {
         var username = $(this).val();
         var csrfToken = $("[name=csrfmiddlewaretoken]").val();
         function csrfSafeMethod(method) {
-            // these HTTP methods do not require CSRF protection
+            /* These HTTP methods do not require CSRF protection. */
             return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
         }
         $.ajaxSetup({
             beforeSend: function(xhr, settings) {
-                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain)
                     xhr.setRequestHeader("X-CSRFToken", csrfToken);
-                }
             }
         });
         $.ajax({
@@ -22,9 +22,35 @@ $('#username_input').ready(function() {
             dataType: 'json',
             success: function(data) {
                 if (data.has_taken) {
-                    alert('Username \"' + username + '\" has already been taken.');
+                    $('#username_invalid_indicator').show();
+                    $('#username_valid_indicator').hide();
+                    $('#username_input').css('border-color', 'red');
+                }
+                else {
+                    $('#username_valid_indicator').show();
+                    $('#username_invalid_indicator').hide();
                 }
             }
         });
     });
+
+    /* Validates the password. */
+    $('#confirm_pswd_input_input').change(function() {
+        var pswd_input = $('#pswd_input');
+        var confirm_pswd_input_input = $(this);
+        if (confirm_pswd_input_input.val() !== pswd_input.val()) {
+            pswd_input.css('border-color', 'red');
+            confirm_pswd_input_input.css('border-color', 'red');
+            $('#pswd_invalid_indicator').show();
+            $('#confirm_pswd_invalid_indicator').show();
+            $('#pswd_valid_indicator').hide();
+            $('#confirm_pswd_valid_indicator').hide();
+        }
+        else {
+            $('#pswd_valid_indicator').show();
+            $('#confirm_pswd_valid_indicator').show();
+            $('#pswd_invalid_indicator').hide();
+            $('#confirm_pswd_invalid_indicator').hide();
+        }
+    })
 });
